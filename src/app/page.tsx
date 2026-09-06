c0e18db62f629b26604735bafbf1dd521ae70b05
akwandokuhlemavimbela8-coder <akwandokuhlemavimbela8@gmail.com>
2026-09-06 17:40:12 +0200

    Add HabitHeatmap component to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Dashboard from '@/components/Dashboard';
 import FocusTimer from '@/components/FocusTimer';
+import HabitHeatmap from '@/components/HabitHeatmap';
 import CommandPalette from '@/components/CommandPalette';
 import Navbar from '@/components/Navbar';
 
@@ -9,7 +10,10 @@ export default function Home() {
       <Navbar />
       <CommandPalette />
       <div className="max-w-4xl mx-auto px-6 pt-8 space-y-8">
-        <FocusTimer />
+        <div className="grid md:grid-cols-2 gap-6">
+          <FocusTimer />
+          <HabitHeatmap />
+        </div>
         <Dashboard />
       </div>
     </main>
